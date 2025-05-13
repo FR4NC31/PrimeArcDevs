69fab85144704061609fa69f8e91569f502dd5a7
Your Name <you@example.com>
2025-05-13 14:57:11 +0800

    fixing bugs

--- src/components/platform.jsx
+++ src/components/platform.jsx
@@ -12,17 +12,17 @@ function platform() {
       <div className='w-90 h-120 shadow-xl flex flex-col justify-center rounded-2xl items-center'>
         <img src={WebDev} className='w-55 h-55'/>
         <h1 className='font-extrabold font-poppins text-lg pt-5 '>Web Development</h1>
-         <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
+         <Link to="/services"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
       </div>
       <div className='w-90 h-120 shadow-xl flex flex-col justify-center rounded-2xl items-center'>
         <img src={MobileDev} className='w-55 h-55'/>
         <h1 className='font-extrabold font-poppins text-lg pt-5' >Mobile App Development</h1>
-         <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
+         <Link to="/services"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
       </div>
       <div className='w-90 h-120 shadow-xl flex flex-col justify-center rounded-2xl items-center'>
         <img src={WebDesign} className='w-55 h-55'/>
         <h1 className='font-extrabold font-poppins text-lg pt-5'>Static Websites</h1>
-         <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
+         <Link to="/services"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
       </div>
     </div>
   )
