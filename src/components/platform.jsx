9b3413b89bdb9b7e4ef35018891386e51926d958
Your Name <you@example.com>
2025-05-13 14:54:39 +0800

    adding platforms and others

--- src/components/platform.jsx
+++ src/components/platform.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Images
 import WebDev from "../assets/images/web-development 1.png"
@@ -7,10 +8,22 @@ import WebDesign from "../assets/images/web-design 1.png"
 
 function platform() {
   return (
-    <div>
-      <img src={WebDev}/>
-      <img src={MobileDev}/>
-      <img src={WebDesign}/>
+    <div className='flex flex-row gap-20 mt-20'>
+      <div className='w-90 h-120 shadow-xl flex flex-col justify-center rounded-2xl items-center'>
+        <img src={WebDev} className='w-55 h-55'/>
+        <h1 className='font-extrabold font-poppins text-lg pt-5 '>Web Development</h1>
+         <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
+      </div>
+      <div className='w-90 h-120 shadow-xl flex flex-col justify-center rounded-2xl items-center'>
+        <img src={MobileDev} className='w-55 h-55'/>
+        <h1 className='font-extrabold font-poppins text-lg pt-5' >Mobile App Development</h1>
+         <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
+      </div>
+      <div className='w-90 h-120 shadow-xl flex flex-col justify-center rounded-2xl items-center'>
+        <img src={WebDesign} className='w-55 h-55'/>
+        <h1 className='font-extrabold font-poppins text-lg pt-5'>Static Websites</h1>
+         <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-46 h-12 text-2xl'>More</button></Link>
+      </div>
     </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,6 @@ import Navbar from '../components/navbar'
 import Platform from '../components/platform'
 
 //Images
-import Logo from "../assets/images/logo.png"
 import Devs from "../assets/images/developers 1.png"
 
 function home() {
@@ -13,9 +12,8 @@ function home() {
     <div>
         <Navbar />
       <main className='flex flex-col justify-center items-center'>
-        <img src={Logo} className='w-180 absolute mb-120 '/>
         <div className='text-center mt-100'>
-          <h1 className='text-8xl font-bold font-audiowide text-violet-500'>PrimeArc Devs</h1>
+          <h1 className='text-8xl font-bold font-audiowide text-violet-500 flex gap-5'>PrimeArc <h1 className='text-black'>Devs</h1></h1>
         <p className='text-2xl font-poppins mt-3'>
           “Innovative solutions for your web and 
            mobile development needs.”
@@ -23,12 +21,15 @@ function home() {
         </div>
         <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-66 h-18 text-3xl'>Contact us</button></Link>
 
-        <p className='text-2xl font-poppins mt-90 text-center '>At PrimeArc Devs, we specialize in creating impactful digital solutions tailored to meet your business needs. From elegant websites to powerful mobile apps, we're here to bring your ideas to life.</p>
+        <p className='text-3xl font-poppins mt-90 text-center pl-50 pr-50'>At PrimeArc Devs, we specialize in creating impactful digital solutions tailored to meet your business needs. From elegant websites to powerful mobile apps, we're here to bring your ideas to life.</p>
 
         <Platform />
 
-        <div>
-          <p>Need a Website or Mobile App? Let's Build It Together!</p>
+        <div className='flex flex-row justify-center items-center gap-20 mt-30'>
+          <div className=''>
+            <p className='font-poppins font-extrabold text-4xl'>Need a Website or Mobile App? <br /> Let's Build It Together!</p>
+            <Link to="/contact"><button className='bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded-full mt-10 w-76 h-18 text-3xl'>Start Your Project</button></Link>
+          </div>
           <img src={Devs} />
         </div>
       </main>
